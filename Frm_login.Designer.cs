e2a652d1babac102eeb869cafb863b5e3b906855
Felipe de Abreu Ferrarini <ferrarinifelipe@gmail.com>
2020-05-28 22:39:19 -0300

    Inicio

---
+++ Frm_login.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Frm_login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_login = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_user
+            // 
+            this.txt_user.AccessibleDescription = "sdfsdf";
+            this.txt_user.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_user.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txt_user.Location = new System.Drawing.Point(596, 215);
+            this.txt_user.Margin = new System.Windows.Forms.Padding(10);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(226, 20);
+            this.txt_user.TabIndex = 0;
+            this.txt_user.Tag = "";
+            // 
+            // txt_password
+            // 
+            this.txt_password.AcceptsTab = true;
+            this.txt_password.Location = new System.Drawing.Point(596, 265);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(226, 20);
+            this.txt_password.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(596, 319);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(226, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.baseline_lock_black_48dp;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(568, 265);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.baseline_person_black_48dp;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.baseline_person_black_48dp;
+            this.pictureBox3.Location = new System.Drawing.Point(568, 215);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.signin_f21363a7;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(428, 297);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb_login
+            // 
+            this.lb_login.AutoSize = true;
+            this.lb_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_login.Location = new System.Drawing.Point(606, 99);
+            this.lb_login.Name = "lb_login";
+            this.lb_login.Size = new System.Drawing.Size(195, 73);
+            this.lb_login.TabIndex = 8;
+            this.lb_login.Text = "Login";
+            // 
+            // Frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(921, 450);
+            this.Controls.Add(this.lb_login);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_user);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Frm_login";
+            this.Text = "Tela Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lb_login;
+    }
+}
+
---
+++ Frm_login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frm_login : Form
+    {
+        Thread t1;
+        public Frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (txt_user.Text=="FELIPE" && txt_password.Text== "lipelipe")
+            {
+                this.Close();
+                t1 = new Thread(abrirJanela);
+                t1.SetApartmentState(ApartmentState.STA);
+                t1.Start();
+            }
+            else
+            {
+                DialogResult info;
+                info = MessageBox.Show("Usuário ou senha incorretos", "Erro");
+            }
+        }
+        private void abrirJanela(object obj)
+        {
+            Application.Run(new Frm_main());
+        }
+    }
+}
